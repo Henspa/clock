d9c74252b8331c8203ac880c12408678f97ad636
henni saaranen <n0sahe00@students.oamk.fi>
2021-09-19 17:47:07 +0300

    vie kello githubiin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import uuid from 'react-uuid';
 
-function App() {
+/* excercise. Clock using hooks
+
+  function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    console.log("ajastin käynnistetty");
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return() => {
+      clearInterval(id);
+    };
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin: '30px'}}>
+      {time.toLocaleTimeString()}
+    </div>
+  );
+}*/
+
+
+// Excercise. List
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const names = ['Jouni', 'Reima', 'Meija', 'Jouni'];
+    setItems(names);
+  }, [])
+
+
+return (
+    <div style={{margin: '30px'}}>
+      <h3>Names</h3>
+      <ul>
+        {items.map(item => (
+          <li key={uuid()}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
